fec21e1e9c4d8e9ef82344651de7d4941bdc8fca
Sergey <63705844+sillva7@users.noreply.github.com>
2020-05-23 19:00:14 +0500

    Create Util.java

---
+++ Util.java
@@ -0,0 +1,17 @@
+package Utils;
+
+public class Util {
+
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "studentsDB";
+    public static final String TABLE_NAME = "Students";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_FACULTY = "faculty";
+    public static final String KEY_LAST_NAME = "last_name";
+    public static final String KEY_FIRST_NAME = "first_name";
+    public static final String KEY_AV_GRADE = "av_grade";
+
+
+}
